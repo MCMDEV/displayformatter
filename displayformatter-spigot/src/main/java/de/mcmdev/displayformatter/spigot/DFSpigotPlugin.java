fa8af44c51b6be4709fdbca0411c1783ddb7d7b8
MCMDEV <john-m.1@gmx.de>
2021-07-13 14:03:04 +0200

    Added bStats integration

--- displayformatter-spigot/src/main/java/de/mcmdev/displayformatter/spigot/DFSpigotPlugin.java
+++ displayformatter-spigot/src/main/java/de/mcmdev/displayformatter/spigot/DFSpigotPlugin.java
@@ -1,6 +1,7 @@
 package de.mcmdev.displayformatter.spigot;
 
 import de.mcmdev.displayformatter.spigot.listeners.PlayerListener;
+import de.mcmdev.displayformatter.spigot.utils.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DFSpigotPlugin extends JavaPlugin {
@@ -12,6 +13,7 @@ public class DFSpigotPlugin extends JavaPlugin {
         this.displayFormatterSpigot = new DisplayFormatterSpigot();
 
         getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+        new Metrics(this, 12030);
     }
 
     public DisplayFormatterSpigot getDisplayFormatter() {
